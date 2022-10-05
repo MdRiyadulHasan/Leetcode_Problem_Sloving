dcaaba756aa17c3957e35d20649a4ca86a922b33
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-05 13:23:52 +0600

    Update two_sum.py

--- Array/two_sum.py
+++ Array/two_sum.py
@@ -1,8 +1,14 @@
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self, nums, target):
         hash_map={}
         for i, n in enumerate(nums):
             diff=target-n
             if diff in hash_map:
                 return [hash_map[diff],i]
             hash_map[n]=i
+if __name__ == '__main__':
+    s=Solution()
+    target=int(input())
+    nums=list(map(int,input().strip().split()))
+    result= s.twoSum(nums,target)
+    print(result)
