8f13810cf2b3b8321ad42d30e124dcc91b9901f0
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-07 01:31:37 +0600

    Create number_1_bits_191.py

---
+++ Bit_Manipulation/number_1_bits_191.py
@@ -0,0 +1,11 @@
+# bit-manipulation
+# apple | microsoft
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result=0
+        while n:
+            if n&1:
+                result+=1
+            n>>=1
+        return result
+        
