f10f7eafc2ce367a25300aac7bcc4d6b290bb581
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-09 22:43:22 +0600

    Create search_insert_position_35.py

---
+++ search_insert_position_35.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        l=0
+        r=len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                return mid
+            if target>nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        return l
+if __name__ == '__main__':
+    ob=Solution()
+    nums=list(map(int, input().strip().split()))
+    target=int(input())
+    ans=ob.searchInsert(nums,target)
+    print(ans)
