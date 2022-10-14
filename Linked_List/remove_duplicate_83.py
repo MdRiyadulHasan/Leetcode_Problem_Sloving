c8590cd54b4aa65dca9a9b481be2918e3ee9f576
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-14 09:30:14 +0600

    Create remove_duplicate_83.py

---
+++ Linked_List/remove_duplicate_83.py
@@ -0,0 +1,14 @@
+# linked-list
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def deleteDuplicates(self, head):
+        cur=head
+        while cur:
+            while cur.next and cur.val==cur.next.val:
+                cur.next=cur.next.next
+            cur=cur.next
+        return head
