dfc27691f1293be5942d6293f56c3e0dcfd4d3a2
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-14 07:42:10 +0600

    Create climbing_stairs.py

---
+++ Dynamic_Programming/climbing_stairs.py
@@ -0,0 +1,15 @@
+# dynamic-programming
+# adobe | apple
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one,two=1,1
+        for i in range (n-1):
+            temp=one
+            one=one+two
+            two=temp
+        return one
+if __name__ == '__main__':
+    n=int(input())
+    s=Solution()
+    ans=s.climbStairs(n)
+    print(ans)
