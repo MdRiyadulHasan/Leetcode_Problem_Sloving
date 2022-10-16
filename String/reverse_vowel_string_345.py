e894fbd40edf65688b2e41633391d4c939129fed
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-16 14:01:40 +0600

    Create reverse_vowel_string_345.py

---
+++ String/reverse_vowel_string_345.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowel=set('aeiouAEIOU')
+        s=list(s)
+        l=0
+        r=len(s)-1
+        while l<r:
+            while l<r and s[l] not in vowel:
+                l=l+1
+            while l<r and s[r] not in vowel:
+                r=r-1
+            s[l],s[r]=s[r],s[l]
+            l,r=l+1, r-1
+        return ''.join(s)
