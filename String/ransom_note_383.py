021db22385ddee324d1d9acc6011ba5b0112900d
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-16 18:10:04 +0600

    Create ransom_note_383.py

---
+++ String/ransom_note_383.py
@@ -0,0 +1,17 @@
+# string
+# apple
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d={}
+        for i in magazine:
+            if i in d:
+                d[i]=d[i]+1
+            else:
+                d[i]=1
+        for i in ransomNote:
+            if i in d and d[i]>0:
+                d[i]=d[i]-1
+            else:
+                return False
+        return True
+        
