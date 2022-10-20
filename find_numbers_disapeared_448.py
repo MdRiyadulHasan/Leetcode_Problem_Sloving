6836a64f3b8e5c9853df2662c60a089ef1681959
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-20 22:21:48 +0600

    Create find_numbers_disapeared_448.py

---
+++ find_numbers_disapeared_448.py
@@ -0,0 +1,13 @@
+# array
+# google
+class Solution:
+    def pivotIndex(self, nums) -> int:
+        total = sum(nums)
+        leftsum=0
+        for i, val in enumerate(nums):
+            rightsum=total-leftsum-val
+            if leftsum==rightsum:
+                return i
+            leftsum=leftsum+val
+        return -1
+        
