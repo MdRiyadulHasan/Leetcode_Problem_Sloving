80a9896b7cd58587c39122ef42e654c28c9b222d
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-28 20:02:06 +0600

    Create simplify_path_71.py

---
+++ Stack/simplify_path_71.py
@@ -0,0 +1,11 @@
+# string | stack
+# facebook | microsoft
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack=[]
+        for elem in path.split('/'):
+            if stack and elem=='..':
+                stack.pop()
+            elif elem not in ['.','','..']:
+                stack.append(elem)
+        return '/'+'/'.join(stack)
