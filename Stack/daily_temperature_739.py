fbc5ac0ae3cca58f3123f70523661fafc2543cc3
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-10-28 17:30:49 +0600

    Create daily_temperature_739.py

---
+++ Stack/daily_temperature_739.py
@@ -0,0 +1,11 @@
+# hash-table | stack
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        stack=[]
+        res=[0]*len(temperatures)
+        for i,t in enumerate(temperatures):
+            while stack and t>stack[-1][0]:
+                stackT,stackInd=stack.pop()
+                res[stackInd]=(i-stackInd)
+            stack.append([t,i])
+        return res
