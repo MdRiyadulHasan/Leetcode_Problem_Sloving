d2623c6ed82e3e5c549592024a7f7624fd602b56
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-11-14 14:00:27 +0600

    Create rotate_image_48.py

---
+++ Array/rotate_image_48.py
@@ -0,0 +1,19 @@
+# array
+# amazon | apple | microsoft
+def rotate(matrix) -> None:
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+    l = 0
+    r = len(matrix)-1
+    while l<r:
+        for i in range(r-l):
+            top = l
+            bottom = r 
+            topLeft = matrix[top][l+i]
+            matrix[top][l+i]=matrix[bottom-i][l]
+            matrix [bottom-i][l] = matrix[bottom][r-i]
+            matrix[bottom][r-i]= matrix[top+i][r]
+            matrix[top+i][r]=topLeft
+        l+=1
+        r-=1
