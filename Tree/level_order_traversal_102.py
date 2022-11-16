59c45b0c42ab8db10af8300d954231851a078bf8
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-11-16 21:12:09 +0600

    Create level_order_traversal_102.py

---
+++ Tree/level_order_traversal_102.py
@@ -0,0 +1,26 @@
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root):
+        if root is None:
+            return root
+        q=deque()
+        q.append(root)
+        res = []
+        while len(q)>0:
+            level = []
+            l =len(q)
+            for i in range(l):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+        return res
+            
